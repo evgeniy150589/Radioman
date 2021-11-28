1b5465f9db87500712d01dabcbe5ab64da0f5d52
evgeniy150589 <epritula89@mail.ru>
2021-11-28 20:04:02 +0300

    RadioMan2_flexible

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
-
+    Radio radio = new Radio();
 
     @Test
     public void shouldTheRadioStationTurnOn6() {
-        Radio radio = new Radio();
-
         radio.setRadioStation(6);
         int expected = 6;
 
@@ -20,10 +18,8 @@ class RadioTest {
 
     @Test
     public void shouldTheRadioStationTurnMinus1() {
-        Radio radio = new Radio();
-
         radio.setRadioStation(-1);
-        int expected = 0;
+        int expected = 9;
 
         int actual = radio.getRadioStation();
         assertEquals(expected, actual);
@@ -32,8 +28,6 @@ class RadioTest {
 
     @Test
     public void shouldTheRadioStationTurnOn12() {
-        Radio radio = new Radio();
-
         radio.setRadioStation(12);
         int expected = 0;
 
@@ -44,7 +38,6 @@ class RadioTest {
 
     @Test
     public void shouldTheRadioStationBeMore9() {
-        Radio radio = new Radio();
         radio.setRadioStation(9);
 
         radio.nextStation();
@@ -58,7 +51,6 @@ class RadioTest {
     @Test
 
     public void shouldTheRadioStationSwitchFrom6To7() {
-        Radio radio = new Radio();
         radio.setRadioStation(6);
 
         radio.nextStation();
@@ -71,7 +63,6 @@ class RadioTest {
 
     @Test
     public void shouldTheRadioStationBeLess0() {
-        Radio radio = new Radio();
         radio.setRadioStation(0);
 
         radio.prevStation();
@@ -84,7 +75,6 @@ class RadioTest {
 
     @Test
     public void shouldTheRadioStationSwitchFrom5To4() {
-        Radio radio = new Radio();
         radio.setRadioStation(5);
 
         radio.prevStation();
@@ -97,7 +87,6 @@ class RadioTest {
 
     @Test
     public void shouldTheVolumeTurnOnTo6() {
-        Radio radio = new Radio();
 
         radio.setVolumeLevel(6);
         int expented = 6;
@@ -107,7 +96,6 @@ class RadioTest {
 
     @Test
     public void shouldTheVolumeTurnOnTo10() {
-        Radio radio = new Radio();
 
         radio.setVolumeLevel(10);
         int expented = 10;
@@ -117,7 +105,6 @@ class RadioTest {
 
     @Test
     public void shouldTheVolumeTurnOnMinus1() {
-        Radio radio = new Radio();
 
         radio.setVolumeLevel(-1);
         int expented = 0;
@@ -127,20 +114,18 @@ class RadioTest {
 
     @Test
     public void shouldTheVolumeTurnOnTo12() {
-        Radio radio = new Radio();
 
         radio.setVolumeLevel(12);
 
         radio.increaseVolume();
 
-        int expented = 10;
+        int expented = 13;
         int actual = radio.getVolumeLevel();
         assertEquals(expented, actual);
     }
 
     @Test
     public void shouldTheVolumeIncreaseFrom1To2() {
-        Radio radio = new Radio();
 
         radio.setVolumeLevel(1);
 
@@ -153,35 +138,32 @@ class RadioTest {
 
 
     @Test
-    public void shouldTheVolumeReductionFrom5To4() {
-        Radio radio = new Radio();
+    public void shouldTheVolumeReductionFrom59To58() {
 
-        radio.setVolumeLevel(5);
+        radio.setVolumeLevel(59);
 
         radio.reductionVolume();
 
-        int expented = 4;
+        int expented = 58;
         int actual = radio.getVolumeLevel();
         assertEquals(expented, actual);
     }
 
 
     @Test
-    public void shouldTheVolumeIncreaseFrom10To11() {
-        Radio radio = new Radio();
+    public void shouldTheVolumeIncreaseFrom100To101() {
 
-        radio.setVolumeLevel(10);
+        radio.setVolumeLevel(100);
 
         radio.increaseVolume();
 
-        int expented = 10;
+        int expented = 100;
         int actual = radio.getVolumeLevel();
         assertEquals(expented, actual);
     }
 
     @Test
     public void shouldTheVolumeDecreaseFrom0() {
-        Radio radio = new Radio();
 
         radio.setVolumeLevel(0);
 
